9faf02375ab8dd800e0f972dbfb042edc3be3061
xianfu <hi.xianfu@gmail.com>
2025-01-02 17:40:19 +0800

    chore: complete create question dto

--- src/question/dto/create-question.dto.ts
+++ src/question/dto/create-question.dto.ts
@@ -1,11 +1,14 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { QuestionType } from "../entities/question.entity";
+import { QuestionType, QuizType } from "../entities/question.entity";
 import { IsNotEmpty } from "class-validator";
 
 export class CreateQuestionDto {
 
     @ApiProperty({ description: '类型' })
     type: QuestionType;
+
+    @ApiProperty({ description: '题目类型' })
+    quizType: QuizType;
 
     @ApiProperty({ description: '题目' })
     question: string;
@@ -14,9 +17,15 @@ export class CreateQuestionDto {
     options: string[];
 
     @ApiProperty({ description: '正确答案' })
-    correct_answer: string;
+    correctAnswer: string;
 
     @IsNotEmpty()
-    @ApiProperty({ description: '课程ID' })
-    courseId: number;
+    @ApiProperty({ description: '关卡ID' })
+    levelId: number;
+
+    @ApiProperty({ description: '该题的分数' })
+    points: number
+
+    @ApiProperty({ description: '是否激活' })
+    isActive: number;
 }
